c1323bf609b55adc2dbb35aafba178a2bf6be7ac
Joonas Loppi <joonas@joonas.fi>
2021-02-09 16:32:29 +0200

    fix Peek settings customization in pre-running environment

--- install.sh
+++ install.sh
@@ -247,9 +247,9 @@ function installTerminalEmulator {
 function installPeek {
 	apt install -y peek
 
-	su "$username" -c "dconf write /com/uploadedlobster/peek/persist-save-folder \"'/tmp'\""
-
-	su "$username" -c "dconf write /com/uploadedlobster/peek/recording-framerate 24"
+	# 'dbus-launch --exit-with-session' prefix: https://askubuntu.com/a/311988
+	su "$username" -c "dbus-launch --exit-with-session gsettings set com.uploadedlobster.peek persist-save-folder /tmp"
+	su "$username" -c "dbus-launch --exit-with-session gsettings set com.uploadedlobster.peek recording-framerate 24"
 }
 
 # When monitors connect/disconnects, set appropriate screen configuration automatically
