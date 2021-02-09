b9159b6ae97a894e0326b6cfbc30d554a01be14c
Joonas Loppi <joonas@joonas.fi>
2021-02-09 15:56:52 +0200

    fix Wireguard installation issue that overrides our symlink

--- install.sh
+++ install.sh
@@ -314,6 +314,12 @@ function installCroc {
 
 function installWireguard {
 	apt install -y wireguard
+
+	# installation unfortunately replaces our symlink with an empty dir
+	if [ ! -L /etc/wireguard ]; then # not a symlink? => replace our symlink back
+		rm -rf /etc/wireguard
+		cp -a "${repodir}/overrides/etc/wireguard" /etc/wireguard
+	fi
 }
 
 # Centralized file storage
