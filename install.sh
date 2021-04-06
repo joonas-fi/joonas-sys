e72b855b40d69d69800748543d154787a88a2b36
Joonas Loppi <joonas@joonas.fi>
2021-04-06 14:12:30 +0300

    support empty directory overrides

--- install.sh
+++ install.sh
@@ -129,7 +129,9 @@ function createUser {
 function overlayOverrides() {
 	apt install -y rsync
 
-	rsync -a "${repodir}/overrides/" /
+	# .empty_dir b/c Git can't track empty directories (without placing dummy file in it), but sometimes
+	# an actual empty directory is important (like mount points), so we
+	rsync --exclude=.empty_dir -a "${repodir}/overrides/" /
 
 	# any file we wrote under user's home dir, we wrote as root
 	chown -R "$username:$username" "/home/$username"
