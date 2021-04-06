35bdaa47896effe8848e4b1c2ff3c749f8d37b6f
Joonas Loppi <joonas@joonas.fi>
2021-04-06 12:35:02 +0300

    james: use local dev copy (instead of downloading)

--- install.sh
+++ install.sh
@@ -391,7 +391,8 @@ function installMkcert {
 
 # Docker cluster management etc.
 function installJames {
-	downloadAndInstallSingleBinaryProgram /usr/bin/james "https://bintray.com/function61/dl/download_file?file_path=james%2F20190628_1117_a7803323%2Fjames_linux-amd64"
+	ln -s /persist/work/james/rel/james_linux-amd64 /usr/bin/james
+	# downloadAndInstallSingleBinaryProgram /usr/bin/james "https://bintray.com/function61/dl/download_file?file_path=james%2F20190628_1117_a7803323%2Fjames_linux-amd64"
 }
 
 # Podcast, Youtube etc. hoarding
