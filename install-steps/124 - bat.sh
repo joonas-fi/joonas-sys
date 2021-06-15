ec04950125ddf6dfd4d3656b0ac8ab25fc9b3029
Joonas Loppi <joonas@joonas.fi>
2021-06-15 15:13:30 +0300

    add bat

---
+++ install-steps/124 - bat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source common.sh
+
+# --wildcards because the binary's inside a dynamically named binary
+curl -fsSL \
+	https://function61.com/app-dl/api/github.com/sharkdp/bat/latest_releases_asset/bat-%2A-x86_64-unknown-linux-gnu.tar.gz \
+	| tar -xz -C /usr/bin -f - --strip-components=1 --wildcards 'bat-*/bat'
+
+#versioncommand: bat --version | cut -d' ' -f2
