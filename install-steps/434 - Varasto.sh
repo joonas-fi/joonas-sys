7f687fd2660bee554212e65265d92f7786c190e3
Joonas Loppi <joonas@joonas.fi>
2021-06-15 15:15:00 +0300

    fix /sto mount point owner

--- install-steps/434 - Varasto.sh
+++ install-steps/434 - Varasto.sh
@@ -9,3 +9,6 @@ downloadAndInstallSingleBinaryProgram \
 	https://function61.com/app-dl/api/github.com/function61/varasto/latest_releases_asset/sto_linux-amd64
 
 #versioncommand: sto --version
+
+# FUSE mount point needs to be owned by us
+chown "$username:$username" /sto
