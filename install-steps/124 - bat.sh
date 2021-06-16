a1b189f157b7df1a097feb2f946171c5440e219f
Joonas Loppi <joonas@joonas.fi>
2021-06-16 14:31:26 +0300

    fix typo

--- install-steps/124 - bat.sh
+++ install-steps/124 - bat.sh
@@ -2,7 +2,7 @@
 
 source common.sh
 
-# --wildcards because the binary's inside a dynamically named binary
+# --wildcards because the binary's inside a dynamically named directory
 curl -fsSL \
 	https://function61.com/app-dl/api/github.com/sharkdp/bat/latest_releases_asset/bat-%2A-x86_64-unknown-linux-gnu.tar.gz \
 	| tar -xz -C /usr/bin -f - --strip-components=1 --wildcards 'bat-*/bat'
