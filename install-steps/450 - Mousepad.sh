0e92ec4aa04b38217375979edda1e23edf27c134
Joonas Loppi <joonas@joonas.fi>
2021-06-17 13:34:54 +0300

    mousepad: open plaintext files with it (instead of LibreOffice)

--- install-steps/450 - Mousepad.sh
+++ install-steps/450 - Mousepad.sh
@@ -5,4 +5,7 @@ source common.sh
 
 apt install -y mousepad
 
+# use for .txt etc files (so they won't open in LibreOffice)
+su "$username" -c "xdg-mime default mousepad.desktop text/plain"
+
 #versioncommand: mousepad --version
