e93a51034f9713af8bc77d3aa91f930bf49ab3e1
Joonas Loppi <joonas@joonas.fi>
2021-10-15 21:51:09 +0300

    fix WPA supplicant

--- install-steps/134 - wpasupplicant.sh
+++ install-steps/134 - wpasupplicant.sh
@@ -5,4 +5,8 @@ source common.sh
 
 apt install -y wpasupplicant
 
+# interferes with per-interface supplicants (netplan related?)
+# disabling is not enough, as this is maybe invoked from a dependency?
+systemctl mask wpa_supplicant
+
 #versioncommand: apt show wpasupplicant | grep Version: | cut -d' ' -f2
