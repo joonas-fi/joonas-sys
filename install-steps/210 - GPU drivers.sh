4597be6c4e88eb160884d24941e2c24c6c7c3bb2
Joonas Loppi <joonas@joonas.fi>
2021-10-15 21:28:14 +0300

    add Nvidia proprietary drivers

--- install-steps/210 - GPU drivers.sh
+++ install-steps/210 - GPU drivers.sh
@@ -2,7 +2,11 @@
 
 source common.sh
 
-
 # somewhere along the process AMD GPU drivers seem to get automatically pulled in..
-# nothing explicit implemented.
-exit 0
+# so do nouveau drivers (Nvidia open source drivers).
+
+# we've to add Nvidia proprietary driver, because nouveau doesn't support video decoding acceleration for GeForce GT 1030
+apt install -y nvidia-driver-460
+
+# info utility for video acceleration debug
+apt install -y vainfo
