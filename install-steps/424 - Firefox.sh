7d103955b3baaee24332a532f6d866538cdb5d16
Joonas Loppi <joonas@joonas.fi>
2021-11-20 18:31:28 +0200

    firefox: xdg-settings does seem like it needs to be added explicitly

--- install-steps/424 - Firefox.sh
+++ install-steps/424 - Firefox.sh
@@ -5,7 +5,7 @@ source common.sh
 
 # libavcodec-extra b/c some website video players aren't working:
 #   https://askubuntu.com/questions/1035661/playing-videos-in-firefox
-apt install -y firefox libavcodec-extra
+apt install -y firefox libavcodec-extra xdg-utils
 
 # https://askubuntu.com/questions/16621/how-to-set-the-default-browser-from-the-command-line
 xdg-settings set default-web-browser firefox.desktop
