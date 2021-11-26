7acd03535762fce2a12fd8a69eac04e2d50b3bc8
Joonas Loppi <joonas@joonas.fi>
2021-11-26 21:38:17 +0200

    change clipcatd to xfce4-clipman because apparently it doesn't do synchronization

--- install-steps/230 - Clipboard manager.sh
+++ install-steps/230 - Clipboard manager.sh
@@ -5,8 +5,4 @@ source common.sh
 
 # a clipboard manager is required if you don't want clipboard to empty when the program exits where
 # you copied the data from
-curl -fsSL \
-	https://function61.com/app-dl/api/github.com/xrelkd/clipcat/latest_releases_asset/clipcat-%2A-x86_64-unknown-linux-gnu.tar.gz \
-	| tar -xz -C /usr/bin -f - clipcat-menu clipcat-notify clipcatctl clipcatd
-
-#versioncommand: clipcatd --version
+apt install -y xfce4-clipman-plugin
