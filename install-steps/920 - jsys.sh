9cd7de0dd942c3c1edd183ae3e4a14599705e97c
Joonas Loppi <joonas@joonas.fi>
2022-01-01 20:24:03 +0200

    lowdiskspace-checker: smarter installation step

--- install-steps/920 - jsys.sh
+++ install-steps/920 - jsys.sh
@@ -4,3 +4,9 @@ source common.sh
 
 # copy jsys command to the installed system
 cp "${repodir}/rel/jsys_linux-amd64" /usr/bin/jsys
+
+# install lowdiskspace-checker as systemd service and ..
+su "$username" -c "jsys lowdiskspace-checker systemd-units"
+
+# .. enable the timer
+su "$username" -c "systemctl --user enable lowdiskspace-checker.timer"
