e9a944ee6988de014116c3ad89db107c01d2863b
Joonas Loppi <joonas+code@joonas.fi>
2022-02-27 17:14:33 +0200

    statuswidgets: move network bandwidth to own file

--- pkg/statusbar/bar.go
+++ pkg/statusbar/bar.go
@@ -9,11 +9,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"math"
-	"os"
 	"os/exec"
 	"sync/atomic"
-	"time"
 
 	. "github.com/function61/gokit/builtin"
 	"github.com/function61/gokit/log/logex"
@@ -21,7 +18,6 @@ import (
 	"github.com/function61/gokit/sync/taskrunner"
 	"github.com/spf13/cobra"
 	"github.com/vishvananda/netlink"
-	"golang.org/x/sys/unix"
 )
 
 func Entrypoint() *cobra.Command {
@@ -168,140 +164,6 @@ func logic(ctx context.Context, logger *log.Logger) error {
 			doRefresh()
 		}
 	}
-}
-
-func networkPoller(ctx context.Context, internetFacingLinkIdxAtomic *atomic.Value, latestNetworkItem *atomic.Value) error {
-	refreshRate := 5 * time.Second
-
-	var statsPrev *netlink.LinkStatistics
-
-	pollOnce := func() error {
-		internetFacingLink, err := netlink.LinkByIndex(internetFacingLinkIdxAtomic.Load().(int))
-		if err != nil {
-			return err
-		}
-
-		statsNow := *internetFacingLink.Attrs().Statistics // shorthand
-
-		if statsPrev == nil { // first iteration
-			statsPrev = &statsNow
-		}
-
-		// stats difference per e.g. five seconds
-		statsDiff := subtract(statsNow, *statsPrev)
-
-		statsPerSecond := multiply(statsDiff, 1/refreshRate.Seconds())
-
-		largestOfRxOrTx := func() string {
-			if statsPerSecond.RxBytes >= statsPerSecond.TxBytes {
-				return fmt.Sprintf("⬇️ %s/s", toFixedWidthKiloBytesOrMegaBytes(int(statsPerSecond.RxBytes)))
-			} else {
-				return fmt.Sprintf("⬆️ %s/s", toFixedWidthKiloBytesOrMegaBytes(int(statsPerSecond.TxBytes)))
-			}
-		}()
-
-		latestNetworkItem.Store(&barItem{
-			Name: "inetbw",
-			// Instance: internetFacingLinkName,
-			Instance: "",
-			Markup:   "none",
-			FullText: largestOfRxOrTx,
-		})
-
-		statsPrev = &statsNow
-
-		return nil
-	}
-
-	if err := pollOnce(); err != nil { // initial
-		return err
-	}
-
-	refresh := time.NewTicker(refreshRate)
-
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case <-refresh.C:
-			if err := pollOnce(); err != nil {
-				return err
-			}
-		}
-	}
-}
-
-func subtract(a, b netlink.LinkStatistics) netlink.LinkStatistics {
-	return addMul(a, b, -1)
-}
-
-func multiply(a netlink.LinkStatistics, multiplier float64) netlink.LinkStatistics {
-	return addMul(netlink.LinkStatistics{}, a, multiplier)
-}
-
-func addMul(a, b netlink.LinkStatistics, multiplier float64) netlink.LinkStatistics {
-	return netlink.LinkStatistics{
-		RxBytes: a.RxBytes + uint64(multiplier*float64(b.RxBytes)),
-		TxBytes: a.TxBytes + uint64(multiplier*float64(b.TxBytes)),
-	}
-}
-
-// focused in producing fixed-width bandwidth measurements in scale humans are used to reading
-func toFixedWidthKiloBytesOrMegaBytes(size int) string {
-	if size == 0 {
-		return "  0 kB"
-	}
-
-	inKilobytesFloat := fmt.Sprintf("%.1f kB", float64(size)/1024)
-	if len(inKilobytesFloat) <= 6 {
-		return inKilobytesFloat
-	}
-
-	inKilobytes := fmt.Sprintf(" %d kB", int(math.Round(float64(size)/1024)))
-	if len(inKilobytes) <= 6 {
-		return inKilobytes
-	}
-
-	inMegabytesFloat := fmt.Sprintf("%.1f MB", float64(size)/(1024*1024))
-	if len(inMegabytesFloat) <= 6 {
-		return inMegabytesFloat
-	}
-
-	return fmt.Sprintf("%3d MB", int(math.Round(float64(size)/(1024*1024))))
-}
-
-// find the link where the default route points to.
-// this is usually the internet-facing link.
-func getDefaultLinkIndex() (int, error) {
-	routes, err := netlink.RouteList(nil, unix.AF_INET)
-	if err != nil {
-		return 0, err
-	}
-
-	defaultRoutes := []netlink.Route{}
-	for _, route := range routes {
-		if route.Dst == nil { // a default route is one that doesn't have a destination subnet
-			defaultRoutes = append(defaultRoutes, route)
-		}
-	}
-
-	if len(defaultRoutes) < 1 {
-		return 0, os.ErrNotExist
-	}
-
-	return defaultRoutes[0].LinkIndex, nil
-}
-
-// wrapper for storing defaultLinkIndex in an atomic value
-func storeDefaultLinkIndex(to *atomic.Value) error {
-	defaultLinkIndex, err := getDefaultLinkIndex()
-	if err != nil {
-		return err
-	}
-
-	to.Store(defaultLinkIndex)
-
-	return nil
 }
 
 // interactive so .bashrc gets sourced (so we support aliases, functions etc.)
---
+++ pkg/statusbar/networkbw.go
@@ -0,0 +1,149 @@
+package statusbar
+
+// Network bandwidth bar item
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"os"
+	"sync/atomic"
+	"time"
+
+	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
+)
+
+func networkPoller(ctx context.Context, internetFacingLinkIdxAtomic *atomic.Value, latestNetworkItem *atomic.Value) error {
+	refreshRate := 5 * time.Second
+
+	var statsPrev *netlink.LinkStatistics
+
+	pollOnce := func() error {
+		internetFacingLink, err := netlink.LinkByIndex(internetFacingLinkIdxAtomic.Load().(int))
+		if err != nil {
+			return err
+		}
+
+		statsNow := *internetFacingLink.Attrs().Statistics // shorthand
+
+		if statsPrev == nil { // first iteration
+			statsPrev = &statsNow
+		}
+
+		// stats difference per e.g. five seconds
+		statsDiff := subtract(statsNow, *statsPrev)
+
+		statsPerSecond := multiply(statsDiff, 1/refreshRate.Seconds())
+
+		largestOfRxOrTx := func() string {
+			if statsPerSecond.RxBytes >= statsPerSecond.TxBytes {
+				return fmt.Sprintf("⬇️ %s/s", toFixedWidthKiloBytesOrMegaBytes(int(statsPerSecond.RxBytes)))
+			} else {
+				return fmt.Sprintf("⬆️ %s/s", toFixedWidthKiloBytesOrMegaBytes(int(statsPerSecond.TxBytes)))
+			}
+		}()
+
+		latestNetworkItem.Store(&barItem{
+			Name: "inetbw",
+			// Instance: internetFacingLinkName,
+			Instance: "",
+			Markup:   "none",
+			FullText: largestOfRxOrTx,
+		})
+
+		statsPrev = &statsNow
+
+		return nil
+	}
+
+	if err := pollOnce(); err != nil { // initial
+		return err
+	}
+
+	refresh := time.NewTicker(refreshRate)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-refresh.C:
+			if err := pollOnce(); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func subtract(a, b netlink.LinkStatistics) netlink.LinkStatistics {
+	return addMul(a, b, -1)
+}
+
+func multiply(a netlink.LinkStatistics, multiplier float64) netlink.LinkStatistics {
+	return addMul(netlink.LinkStatistics{}, a, multiplier)
+}
+
+func addMul(a, b netlink.LinkStatistics, multiplier float64) netlink.LinkStatistics {
+	return netlink.LinkStatistics{
+		RxBytes: a.RxBytes + uint64(multiplier*float64(b.RxBytes)),
+		TxBytes: a.TxBytes + uint64(multiplier*float64(b.TxBytes)),
+	}
+}
+
+// focused in producing fixed-width bandwidth measurements in scale humans are used to reading
+func toFixedWidthKiloBytesOrMegaBytes(size int) string {
+	if size == 0 {
+		return "  0 kB"
+	}
+
+	inKilobytesFloat := fmt.Sprintf("%.1f kB", float64(size)/1024)
+	if len(inKilobytesFloat) <= 6 {
+		return inKilobytesFloat
+	}
+
+	inKilobytes := fmt.Sprintf(" %d kB", int(math.Round(float64(size)/1024)))
+	if len(inKilobytes) <= 6 {
+		return inKilobytes
+	}
+
+	inMegabytesFloat := fmt.Sprintf("%.1f MB", float64(size)/(1024*1024))
+	if len(inMegabytesFloat) <= 6 {
+		return inMegabytesFloat
+	}
+
+	return fmt.Sprintf("%3d MB", int(math.Round(float64(size)/(1024*1024))))
+}
+
+// find the link where the default route points to.
+// this is usually the internet-facing link.
+func getDefaultLinkIndex() (int, error) {
+	routes, err := netlink.RouteList(nil, unix.AF_INET)
+	if err != nil {
+		return 0, err
+	}
+
+	defaultRoutes := []netlink.Route{}
+	for _, route := range routes {
+		if route.Dst == nil { // a default route is one that doesn't have a destination subnet
+			defaultRoutes = append(defaultRoutes, route)
+		}
+	}
+
+	if len(defaultRoutes) < 1 {
+		return 0, os.ErrNotExist
+	}
+
+	return defaultRoutes[0].LinkIndex, nil
+}
+
+// wrapper for storing defaultLinkIndex in an atomic value
+func storeDefaultLinkIndex(to *atomic.Value) error {
+	defaultLinkIndex, err := getDefaultLinkIndex()
+	if err != nil {
+		return err
+	}
+
+	to.Store(defaultLinkIndex)
+
+	return nil
+}
