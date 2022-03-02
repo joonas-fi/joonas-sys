ad2fc05d01164db888235332ca647b22989f8ddc
Joonas Loppi <joonas+code@joonas.fi>
2022-03-02 22:28:29 +0200

    i3 workspace change script

---
+++ overrides/usr/bin/workspace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+# from https://github.com/davatorium/rofi-scripts/blob/master/i3_switch_workspace.sh
+
+function gen_workspaces()
+{
+    i3-msg -t get_workspaces | tr ',' '\n' | grep "name" | sed 's/"name":"\(.*\)"/\1/g' | sort -n
+}
+
+
+WORKSPACE=$( (echo empty; gen_workspaces)  | rofi -dmenu -p "Select workspace:")
+
+if [ x"empty" = x"${WORKSPACE}" ]
+then
+    i3_empty_workspace.sh
+elif [ -n "${WORKSPACE}" ]
+then
+    i3-msg workspace "${WORKSPACE}"
+fi
