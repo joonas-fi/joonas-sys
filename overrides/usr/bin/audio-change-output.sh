84870991c110b42f72ba2154336dc871581eb059
Joonas Loppi <joonas+code@joonas.fi>
2022-03-02 22:28:09 +0200

    audio-change-output.sh: make prettier

--- overrides/usr/bin/audio-change-output.sh
+++ overrides/usr/bin/audio-change-output.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
-sink="$(pactl list short sinks | cut -f 2 | rofi -dpi 1 -dmenu -p "Change audio:")";
+sink="$(pactl list short sinks | cut -f 2 | rofi -dmenu -p "Change audio")";
 
 pacmd set-default-sink "$sink"
