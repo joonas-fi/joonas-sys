8d846f56beb27154d6a0139de4377e6f52b3e553
Joonas Loppi <joonas+code@joonas.fi>
2022-10-01 01:08:03 +0300

    enable PipeWire

---
+++ install-steps/221 - PipeWire.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source common.sh
+
+
+# Ubuntu seems to add pipewire by default (still installing it here for explicitness), but
+# pipewire-pulse needed to take over PulseAudio
+DEBIAN_FRONTEND=noninteractive apt install -y pipewire pipewire-pulse
+
+#versioncommand: apt show pipewire | grep Version: | cut -d' ' -f2
