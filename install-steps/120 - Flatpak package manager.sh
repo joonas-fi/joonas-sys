a903939a88ad1d9c1c2eee6dffc837fab780820d
Joonas Loppi <joonas+code@joonas.fi>
2022-10-01 00:54:12 +0300

    flatpak: store state in persist partition

--- install-steps/120 - Flatpak package manager.sh
+++ install-steps/120 - Flatpak package manager.sh
@@ -8,4 +8,9 @@ apt install -y flatpak
 
 flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# all of this is stateful and needs to be stored in the persist partition.
+# store this under "seed" name so the user can copy the content of this as the initial persist partition.
+mv /var/lib/flatpak /var/lib/flatpak-seed
+ln -s /persist/apps/flatpak /var/lib/flatpak
+
 #versioncommand: flatpak --version | cut -d ' ' -f 2
