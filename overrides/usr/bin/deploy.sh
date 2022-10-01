fd67632be6a118e8a3dfb126f535c2a461055bcb
Joonas Loppi <joonas+code@joonas.fi>
2022-10-01 15:50:46 +0300

    deploy script

---
+++ overrides/usr/bin/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -eu
+
+cd /persist/apps/deployer
+
+# deployments/joonas.fi-blog/ => joonas.fi-blog
+# joonas.fi-blog => (keep as-is)
+# service="$(basename $1)"
+
+service="$(ls -1 deployments/ | rofi -dmenu -p 'Service')"
+
+source env-prod.env
+
+# need this because we're not in a terminal
+i3-sensible-terminal --command rel/deployer_linux-amd64 deploy "$service" ""
+
+# rel/deployer_linux-amd64 deploy "$service" ""
+
+notify-send "Deployed: $service"
