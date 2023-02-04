472848f5c07af4792e480e6184dddf1c1a6c8779
Joonas Loppi <joonas+code@joonas.fi>
2023-02-04 15:47:13 +0200

    QEMU: add RISC-V

--- install-steps/406 - QEMU.sh
+++ install-steps/406 - QEMU.sh
@@ -5,7 +5,8 @@ source common.sh
 
 # Virtual Machines
 # also uses Samba (for VM shared folders), which we install earlier
+# qemu-system-misc for RISC-V and some other exotic architectures.
 
-apt install -y qemu-system-x86
+apt install -y qemu-system-x86 qemu-system-misc
 
 #versioncommand: qemu-system-x86_64 --version
