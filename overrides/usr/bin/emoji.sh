b209e1d7e546cbd3a04c44d22891b82db126228b
Joonas Loppi <joonas+code@joonas.fi>
2023-05-28 00:03:23 +0300

    emoji: dirty hack to better support ASCII art

--- overrides/usr/bin/emoji.sh
+++ overrides/usr/bin/emoji.sh
@@ -2,9 +2,18 @@
 
 emojiAndDescription="$(cat /var/lib/rofimoji-data/* | rofi -dmenu -p 'Emoji')"
 
-# "<emoji> <description>" => "<emoji>"
-emoji="$(echo -n "$emojiAndDescription" | cut -d ' ' -f 1)"
+# unfortunately we have in this directory two different file "formats":
+# 1. emojis with emoji first then description
+# 2. ascii artwork with description first, then artwork
+
+# dirty hack: all emoji descriptions contain "<small>" substring
+if [[ $emojiAndDescription == *"<small>"* ]]; then
+	# "<emoji> <description>" => "<emoji>"
+	emoji="$(echo -n "$emojiAndDescription" | cut -d ' ' -f 1)"
+else
+	# "<description> <tab> <ascii art>" => "<ascii art>"
+	emoji="$(echo -n "$emojiAndDescription" | cut -d '	' -f 2)"
+fi
 
 # copy to clipboard
 echo -n "$emoji" | xclip -selection clipboard
-
