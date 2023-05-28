01a1c485e13017b8794b16e41c3b64fb07938e82
Joonas Loppi <joonas+code@joonas.fi>
2023-05-28 18:44:41 +0300

    pipewire: to be useful, it also needs wireplumber

--- install-steps/221 - PipeWire.sh
+++ install-steps/221 - PipeWire.sh
@@ -5,6 +5,6 @@ source common.sh
 
 # Ubuntu seems to add pipewire by default (still installing it here for explicitness), but
 # pipewire-pulse needed to take over PulseAudio
-DEBIAN_FRONTEND=noninteractive apt install -y pipewire pipewire-pulse
+DEBIAN_FRONTEND=noninteractive apt install -y pipewire pipewire-pulse wireplumber
 
 #versioncommand: apt show pipewire | grep Version: | cut -d' ' -f2
