6a6faef0272622d2f64c7d8b4934e2d739729c0d
Joonas Loppi <joonas+code@joonas.fi>
2023-05-28 13:09:06 +0300

    QEMU: add virtio-fsd

--- install-steps/406 - QEMU.sh
+++ install-steps/406 - QEMU.sh
@@ -2,6 +2,14 @@
 
 source common.sh
 
+installVirtiofsd() {
+	mkdir /tmp/virtiofsd-download
+	cd /tmp/virtiofsd-download
+	# for some reason wget doesn't work (we get 403), but only inside a container
+	curl -fsSL -o virtiofsd.zip "https://gitlab.com/virtio-fs/virtiofsd/-/jobs/artifacts/main/download?job=publish"
+	unzip -j virtiofsd.zip target/x86_64-unknown-linux-musl/release/virtiofsd -d /usr/bin
+	rm -rf /tmp/virtiofsd-download
+}
 
 # Virtual Machines
 # also uses Samba (for VM shared folders), which we install earlier
@@ -10,5 +18,7 @@ source common.sh
 
 apt install -y qemu-system-x86 qemu-system-arm qemu-system-misc \
 	qemu-efi-aarch64
+
+installVirtiofsd
 
 #versioncommand: qemu-system-x86_64 --version
