9efeae62fcb86475c20eee264a12661539ef9247
Joonas Loppi <joonas+code@joonas.fi>
2023-05-28 12:29:13 +0300

    QEMU: add ARM emulators + EFI for `aarch64`

--- install-steps/406 - QEMU.sh
+++ install-steps/406 - QEMU.sh
@@ -6,7 +6,9 @@ source common.sh
 # Virtual Machines
 # also uses Samba (for VM shared folders), which we install earlier
 # qemu-system-misc for RISC-V and some other exotic architectures.
+# qemu-efi-aarch64 to have EFI firmware
 
-apt install -y qemu-system-x86 qemu-system-misc
+apt install -y qemu-system-x86 qemu-system-arm qemu-system-misc \
+	qemu-efi-aarch64
 
 #versioncommand: qemu-system-x86_64 --version
