7896716628435a65fede758cc4f8bbd321f1bbd4
Joonas Loppi <joonas+code@joonas.fi>
2023-05-28 18:44:54 +0300

    pipewire: disable for now (we are not compatible yet)

--- install-steps/221 - PipeWire.sh
+++ install-steps/221 - PipeWire.sh
@@ -3,6 +3,10 @@
 source common.sh
 
 
+# disabled for now because Pipewire doesn't support PulseAudio DBus protocol (which we rely on):
+#   https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/1127)
+exit 0
+
 # Ubuntu seems to add pipewire by default (still installing it here for explicitness), but
 # pipewire-pulse needed to take over PulseAudio
 DEBIAN_FRONTEND=noninteractive apt install -y pipewire pipewire-pulse wireplumber
