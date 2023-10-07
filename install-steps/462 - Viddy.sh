a6aaea917d2ca588543bb58ff344ce80532612fd
Joonas Loppi <joonas+code@joonas.fi>
2023-10-07 13:17:47 +0300

    viddy: release archive naming was changed
    related: #39

--- install-steps/462 - Viddy.sh
+++ install-steps/462 - Viddy.sh
@@ -5,7 +5,7 @@ source common.sh
 
 # A modern watch command. Time machine and pager etc.
 
-curl -fsSL https://function61.com/app-dl/api/github.com/sachaos/viddy/latest_releases_asset/viddy_%2A_Linux_x86_64.tar.gz \
+curl -fsSL "https://function61.com/app-dl/api/github.com/sachaos/viddy/latest_releases_asset/__autodetect__.tar.gz?os=linux&arch=amd64" \
 	| tar -C /usr/bin -xzf - viddy
 
 #versioncommand: viddy --version | cut -d ' ' -f3
