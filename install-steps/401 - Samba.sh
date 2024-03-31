fb9aad60b3d94ce631c5e5eaedb92c7cadf90975
Joonas Loppi <joonas+code@joonas.fi>
2024-04-01 00:03:14 +0300

    samba: improve docs

--- install-steps/401 - Samba.sh
+++ install-steps/401 - Samba.sh
@@ -4,7 +4,8 @@ source common.sh
 
 apt install --no-install-recommends -y samba
 
-# we only install Samba for QEMU (it runs its own instance: doesn't need the background service)
+# we only install Samba for QEMU Samba-based network file share support
+# (it runs its own instance: doesn't need the background service)
 systemctl disable smbd
 
 #versioncommand: samba --version
