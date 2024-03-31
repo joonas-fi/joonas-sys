da40bdba941dd3ef1290e7ccc86a2b0fcdd30d3c
Joonas Loppi <joonas+code@joonas.fi>
2024-03-31 23:20:25 +0300

    i3: switch back to official upstream (it now has gaps support)

--- install-steps/420 - i3.sh
+++ install-steps/420 - i3.sh
@@ -3,23 +3,15 @@
 source common.sh
 
 
-# tiling window manager (gaps fork for prettier visuals)
+# tiling window manager
 
-
-# status bar for i3. recommended would install dzen2 and i3-wm
-# apt install --no-install-recommends -y i3status
-
-# i3-gaps (a fork of i3 with gaps support) exists only as PPA
-add-apt-repository -y ppa:regolith-linux/release
-
-# a horrible hack to get packages for older release (b/c there are no packages for current release)
-echo "deb https://ppa.launchpadcontent.net/regolith-linux/release/ubuntu impish main" > /etc/apt/sources.list.d/regolith-linux-ubuntu-release-jammy.list
-apt update
+# we used to install gaps fork from PPA but it proved a nightmare to always search a new maintained
+# fork for each Ubuntu release. luckily the gaps feature was finally accepted upstream.
 
 # recommended would install dunst.
 # suckless-tools = dmenu (used for workspace renaming)
-# session means setting file so the i3 session shows up in greeter
+# i3status = status bar for i3. recommended would install dzen2 and i3-wm
 apt install --no-install-recommends -y \
-	i3-gaps-wm i3-gaps-session i3status i3lock suckless-tools
+	i3-wm i3status i3lock suckless-tools
 
 #versioncommand: i3 --version
