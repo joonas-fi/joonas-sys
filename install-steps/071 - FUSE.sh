fad26545e2031a587168290c16e10c2a31ce6e4a
Joonas Loppi <joonas+code@joonas.fi>
2024-04-01 00:11:59 +0300

    fuse: cleanup step

--- install-steps/071 - FUSE.sh
+++ install-steps/071 - FUSE.sh
@@ -2,12 +2,9 @@
 
 source common.sh
 
-# the installer will complain if it would try to overwrite our config file.
-# we'll restore this after installation is complete
-rm /etc/fuse.conf
+
+# A filesystem in which data and metadata are provided by an ordinary userspace process. 
 
 DEBIAN_FRONTEND=noninteractive apt install -y fuse
-
-cp "${repodir}/overrides/etc/fuse.conf" /etc/fuse.conf
 
 #versioncommand: fusermount --version | cut -d' ' -f3
