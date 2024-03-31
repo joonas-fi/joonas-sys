3cd517b0f92418ed5c76f0559327ef40064d3862
Joonas Loppi <joonas+code@joonas.fi>
2024-03-31 16:21:48 +0300

    `promptUISelect()` util

--- cmd/jsys/utils.go
+++ cmd/jsys/utils.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/function61/gokit/app/retry"
 	"github.com/function61/gokit/os/osutil"
+	"github.com/manifoldco/promptui"
 )
 
 func waitForFileAvailable(ctx context.Context, file string) error {
@@ -97,4 +98,39 @@ func min(a, b int) int {
 	} else {
 		return b
 	}
+}
+
+// hides the boilerplate of promptui.Select{}.Run()
+func promptUISelect(label string, items []string) (int, string, error) {
+	return (&promptui.Select{
+		Label:    label,
+		Size:     20, // increased from ridiculous minimum
+		Items:    items,
+		Stdout:   &bellSkipper{}, // don't do audible bell on item highlight
+		HideHelp: true,           // hide "Use the arrow keys to navigate: ↓ ↑ → ←"
+	}).Run()
+}
+
+// bellSkipper implements an io.WriteCloser that skips the terminal bell
+// character (ASCII code 7), and writes the rest to os.Stderr. It is used to
+// replace readline.Stdout, that is the package used by promptui to display the
+// prompts.
+//
+// This is a workaround for the bell issue documented in
+// https://github.com/manifoldco/promptui/issues/49.
+type bellSkipper struct{}
+
+// Write implements an io.WriterCloser over os.Stderr, but it skips the terminal
+// bell character.
+func (bs *bellSkipper) Write(b []byte) (int, error) {
+	const charBell = 7 // c.f. readline.CharBell
+	if len(b) == 1 && b[0] == charBell {
+		return 0, nil
+	}
+	return os.Stderr.Write(b)
+}
+
+// Close implements an io.WriterCloser over os.Stderr.
+func (bs *bellSkipper) Close() error {
+	return os.Stderr.Close()
 }
