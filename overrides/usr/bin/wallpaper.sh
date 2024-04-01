15cb60961b84b9d505272e17a5f786d27c181d69
Joonas Loppi <joonas+code@joonas.fi>
2024-04-01 17:11:15 +0300

    wallpaper.sh: resize

--- overrides/usr/bin/wallpaper.sh
+++ overrides/usr/bin/wallpaper.sh
@@ -4,6 +4,18 @@ cd /sto/id/p2xQgboD7cI
 
 wallpaper="$(ls -1 | sort --random-sort | head -1)"
 
-xwallpaper --zoom "$wallpaper"
+mkdir -p /dev/shm/wallpaper-change
+
+cp "$wallpaper" /dev/shm/wallpaper-change/
+
+notify-send "Wallpaper change" "Resizing..."
+
+docker run --rm --net=none -v /dev/shm/wallpaper-change:/workspace joonas/imagemagick convert "$wallpaper" wallpaper.png
+
+xwallpaper --zoom /dev/shm/wallpaper-change/wallpaper.png
+
+cp /dev/shm/wallpaper-change/wallpaper.png /sysroot/apps/SYSTEM/background.png
 
 notify-send "Wallpaper change" "$wallpaper"
+
+rm -rf /dev/shm/wallpaper-change
