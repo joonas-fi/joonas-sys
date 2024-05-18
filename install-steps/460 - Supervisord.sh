1f6c1f89b6c07935b79ebe100177010f6508231a
Joonas Loppi <joonas+code@joonas.fi>
2024-05-18 14:48:43 +0300

    remove supervisord

--- install-steps/460 - Supervisord.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source common.sh
-
-
-# Alternative to Systemd. used with Unikie project for a while.
-apt install -y supervisor
-
-#versioncommand: apt show supervisor | grep Version: | cut -d' ' -f2
