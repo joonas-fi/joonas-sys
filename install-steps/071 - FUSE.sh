a26bebbf3e0fa3ad9136dcf04013ce1a6558ec42
Joonas Loppi <joonas+code@joonas.fi>
2024-07-04 18:16:40 +0300

    fuse: fix for nag about having our own config file

--- install-steps/071 - FUSE.sh
+++ install-steps/071 - FUSE.sh
@@ -5,6 +5,8 @@ source common.sh
 
 # A filesystem in which data and metadata are provided by an ordinary userspace process. 
 
-DEBIAN_FRONTEND=noninteractive apt install -y fuse
+# force-confnew to tell it to keep our custom `/etc/fuse.conf`:
+# https://stackoverflow.com/a/7157196
+DEBIAN_FRONTEND=noninteractive apt -o DPkg::Options::="--force-confnew" install -y fuse
 
 #versioncommand: fusermount --version | cut -d' ' -f3
