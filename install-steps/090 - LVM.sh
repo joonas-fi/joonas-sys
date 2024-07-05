95f50c446339b471fa822eed0dbd2e60e2976350
Joonas Loppi <joonas+code@joonas.fi>
2024-07-05 23:14:30 +0300

    prepare for cryptsetup

--- install-steps/090 - LVM.sh
+++ install-steps/090 - LVM.sh
@@ -4,6 +4,6 @@ source common.sh
 
 
 # Logical Volume Management. snapshots, full-disk encryption etc.
-apt install -y lvm2
+apt install -y lvm2 cryptsetup
 
 #versioncommand: apt show lvm2 | grep Version: | cut -d' ' -f2
