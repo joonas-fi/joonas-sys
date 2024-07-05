4b20ff28780d4575604879362d98896ee8dd5f9b
Joonas Loppi <joonas+code@joonas.fi>
2024-07-05 23:24:28 +0300

    peek: expose host `/tmp`

--- packages-flatpak/com.uploadedlobster.peek/run-after-install.sh
+++ packages-flatpak/com.uploadedlobster.peek/run-after-install.sh
@@ -2,6 +2,7 @@
 
 echo 'gsettings set com.uploadedlobster.peek recording-framerate 24' | flatpak run '--command=sh' com.uploadedlobster.peek
 echo 'gsettings set com.uploadedlobster.peek recording-output-format mp4' | flatpak run '--command=sh' com.uploadedlobster.peek
+echo 'gsettings set com.uploadedlobster.peek persist-save-folder /tmp' | flatpak run '--command=sh' com.uploadedlobster.peek
 
-# TODO: need to give access to Flatpak to /tmp
-# echo 'gsettings set com.uploadedlobster.peek persist-save-folder /tmp' | flatpak run '--command=sh' com.uploadedlobster.peek
+# without this the app doesn't see the host /tmp
+flatpak override --user --filesystem=/tmp com.uploadedlobster.peek
