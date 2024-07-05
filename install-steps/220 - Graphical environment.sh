ff696018bfd1e766e6474ade87539db5663c3296
Joonas Loppi <joonas+code@joonas.fi>
2024-07-06 01:21:38 +0300

    graphical environment: lightdm -> gdm3

--- install-steps/220 - Graphical environment.sh
+++ install-steps/220 - Graphical environment.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-source common.sh
+# here's how i got it working
+# ---------------------------
+# 
+# install gpu-manager (ubuntu-drivers-common)
+# 
+# run prime-switch
+# 
+# remove gdm3
+# 
+# source common.sh
 
+# overridden by us. apt will freak out on package installation if this file already exists, so
+# we remove it. the final steps will put it back.
+rm /etc/gdm3/custom.conf
 
 # (why "noninteractive"? read rant about gdm3 below)
 #
 # unless we specify slick-greeter, it's going to default to unity-greeter which pulls in
 # parts of Unity also
 
-# - lightdm is a display manager (= GUI for logging in to your desktop)
+# - gdm3 is a display manager (= GUI for logging in to your desktop)
 # - rofi is an application launcher
 # - xwallpaper might not be always required (once hautomo-client can set wallpapers without it)
 # - ttf-ancient-fonts because emojis didn't render (https://www.omgubuntu.co.uk/2014/11/see-install-use-emoji-symbols-ubuntu-linux)
@@ -17,10 +29,10 @@ source common.sh
 # - gvfs-fuse = GVFS files to work in non-GIO (= POSIX) programs
 # - xdg-utils = xdg-mime (for setting default programs for file types)
 DEBIAN_FRONTEND=noninteractive apt install -y \
+	gdm3 \
 	xfce4 \
 	xfce4-screensaver \
 	xfce4-notifyd \
-	lightdm \
 	xdg-utils \
 	slick-greeter \
 	gvfs-backends \
@@ -39,10 +51,10 @@ DEBIAN_FRONTEND=noninteractive apt install -y \
 # tried to research which package pulls it, I couldn't figure it out and now we have conflict
 # because we have 2 managers, and now we've to fix it. UGH!
 
-echo /usr/bin/lightdm > /etc/X11/default-display-manager
-
-rm /etc/systemd/system/display-manager.service
-
-ln -s /lib/systemd/system/lightdm.service /etc/systemd/system/display-manager.service
+# echo /usr/bin/lightdm > /etc/X11/default-display-manager
+# 
+# rm /etc/systemd/system/display-manager.service
+# 
+# ln -s /lib/systemd/system/lightdm.service /etc/systemd/system/display-manager.service
 
 #versioncommand: lightdm --version
