154e356b8406756137b3b0b7f606c2f64046b577
Joonas Loppi <joonas+code@joonas.fi>
2024-07-05 20:26:09 +0300

    helix: advertise hx in `$ select-editor`

--- install-steps/463 - Helix.sh
+++ install-steps/463 - Helix.sh
@@ -8,5 +8,8 @@ source common.sh
 curl -fsSL https://function61.com/app-dl/api/github.com/helix-editor/helix/latest_releases_asset/helix-%2A-x86_64-linux.tar.xz \
 	| tar --strip-components=1 -C /usr/bin -xJf - --wildcards '*/hx'
 
+# advertise hx in `$ select-editor`. learn more: https://wiki.debian.org/DebianAlternatives
+update-alternatives --install /usr/bin/editor editor $(which hx) 50
+
 #versioncommand: hx --version | cut -d ' ' -f2
 
