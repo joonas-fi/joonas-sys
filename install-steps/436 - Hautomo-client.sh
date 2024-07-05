90911e1dae09b009302ed4a93cc5ca4b61785797
Joonas Loppi <joonas+code@joonas.fi>
2024-07-05 23:23:12 +0300

    hautomo-client: use dirty hack from our own repo

--- install-steps/436 - Hautomo-client.sh
+++ install-steps/436 - Hautomo-client.sh
@@ -4,6 +4,6 @@ source common.sh
 
 
 # Integrate home automation to my PC
+cp "${repodir}/hautomo-client" /usr/bin/hautomo-client
 
 # downloadAndInstallSingleBinaryProgram /usr/bin/hautomo-client https://function61.com/app-dl/api/github.com/function61/hautomo/latest_releases_asset/...%2A.bin
-exit 0
