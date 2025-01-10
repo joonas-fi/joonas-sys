c0f72d34a98b75fd22785dae050d38bd592b74e7
Joonas Loppi <joonas+code@joonas.fi>
2025-01-10 22:21:25 +0200

    `install-flatpak-apps.sh`: add `--choose` interactive selector for installing just one package

--- overrides/usr/bin/install-flatpak-apps.sh
+++ overrides/usr/bin/install-flatpak-apps.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -eu
 
+usage() {
+	echo "Usage: $0 [--choose] [--all]"
+	echo "  --choose    Perform the 'choose' operation."
+	echo "  --all       Perform the 'all' operation."
+	exit 1
+}
+
 add() {
 	local package="$1"
 
@@ -11,11 +18,27 @@ add() {
 	fi
 }
 
-# dir names in /etc/packages-flatpak/ indicate which packages we should install
-for package_with_path in /etc/packages-flatpak/*
-do
-	# "packages-flatpak/com.brave.Browser" -> "com.brave.Browser"
-	package="$(basename $package_with_path)"
+# Parse command-line arguments
+case "${1:-}" in
+	--choose)
+			package=$(ls -1 /etc/packages-flatpak | fzf)
 
-	add "$package"
-done
+			add "$package"
+		;;
+	--all)
+		# dir names in /etc/packages-flatpak/ indicate which packages we should install
+		for package_with_path in /etc/packages-flatpak/*
+		do
+			# "packages-flatpak/com.brave.Browser" -> "com.brave.Browser"
+			package="$(basename $package_with_path)"
+
+			echo "$package"
+
+			add "$package"
+		done
+		;;
+	*)
+		usage
+		;;
+esac
+
